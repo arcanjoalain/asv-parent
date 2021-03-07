d6199668aae83c1bbc21c5f711e8a448755a8900
Alain S Vieira <alain.vieira@institutoitn.com.br>
2021-03-07 08:36:31 -0400

    initial

---
+++ src/main/java/br/com/asv/parent/AsvParentApplication.java
@@ -0,0 +1,13 @@
+package br.com.asv.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsvParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsvParentApplication.class, args);
+	}
+
+}
